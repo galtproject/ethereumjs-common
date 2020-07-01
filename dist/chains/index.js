2092408530c9be684fe41bc9c6e8286b1234922b
jonybang <Jonybange@gmail.com>
2020-07-01 16:17:50 +0200

    add dist

---
+++ dist/chains/index.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.chains = void 0;
+exports.chains = {
+    names: {
+        '1': 'mainnet',
+        '3': 'ropsten',
+        '4': 'rinkeby',
+        '42': 'kovan',
+        '77': 'sokol',
+        '6284': 'goerli',
+    },
+    mainnet: require('./mainnet.json'),
+    ropsten: require('./ropsten.json'),
+    rinkeby: require('./rinkeby.json'),
+    kovan: require('./kovan.json'),
+    sokol: require('./sokol.json'),
+    goerli: require('./goerli.json'),
+};
+//# sourceMappingURL=index.js.map
