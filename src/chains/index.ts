5c3b3b7b1c2e75d9ccf3a8307f6f4bb886a7105d
jonybang <Jonybange@gmail.com>
2020-07-01 16:10:30 +0200

    add sokol chain

--- src/chains/index.ts
+++ src/chains/index.ts
@@ -6,11 +6,13 @@ export const chains: chainsType = {
     '3': 'ropsten',
     '4': 'rinkeby',
     '42': 'kovan',
+    '77': 'sokol',
     '6284': 'goerli',
   },
   mainnet: require('./mainnet.json'),
   ropsten: require('./ropsten.json'),
   rinkeby: require('./rinkeby.json'),
   kovan: require('./kovan.json'),
+  sokol: require('./sokol.json'),
   goerli: require('./goerli.json'),
 }
